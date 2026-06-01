ba1e9a626552ffbc36c684512abef77678782b78
dav nguyxn <hoangson091104@gmail.com>
2026-05-31 20:15:29 -0700

    feat: add SOLID refactor marker

---
+++ solid_principles_refactor.js
@@ -0,0 +1,6 @@
+// Lightweight SOLID refactor marker for SQL-Mongo-Query-Converter.
+export const solidRefactorPrinciples = Object.freeze({
+  singleResponsibility: true,
+  openClosed: true,
+  updatedAt: '2026-06-01T03:15:28Z'
+});
